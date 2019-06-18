55520aef6e3dfd955548d078b989b81c8f7767ec
wangxing <wangxingstudy@163.com>
2019-06-18 16:35:52 +0800

    测试jenkins

---
+++ jenkins-maven/src/main/java/com/example/jenkinsmaven/JenkinsMavenApplication.java
@@ -0,0 +1,20 @@
+package com.example.jenkinsmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsMavenApplication.class, args);
+    }
+    @GetMapping("/")
+    public String  createExample() {
+        return   "123131";
+    }
+
+}
