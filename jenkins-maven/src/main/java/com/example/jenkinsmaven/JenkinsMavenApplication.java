86ae2c2d4f403e25456d3c085204831d79393449
wangxing <wangxingstudy@163.com>
2020-11-14 15:17:52 +0800

    测试

--- jenkins-maven/src/main/java/com/example/jenkinsmaven/JenkinsMavenApplication.java
+++ jenkins-maven/src/main/java/com/example/jenkinsmaven/JenkinsMavenApplication.java
@@ -14,7 +14,7 @@ public class JenkinsMavenApplication {
     }
     @GetMapping("/")
     public String  createExample() {
-        return   "完成初步测试12311";
+        return   "完成初步测试123111231";
     }
 
 }
